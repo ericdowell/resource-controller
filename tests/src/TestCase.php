e187b75c646486056dbebdb3f44d406da827bf76
Eric Dowell <eric@ericdowell.com>
2018-04-18 21:02:36 -0500

    Removing reference to ServiceProvider.php.

--- tests/src/TestCase.php
+++ tests/src/TestCase.php
@@ -2,23 +2,10 @@
 
 namespace EricDowell\ResourceController\Tests;
 
-use EricDowell\ResourceController\ServiceProvider;
 use Orchestra\Testbench\TestCase as SupportTestCase;
 use EricDowell\ResourceController\Tests\Traits\LoadTestConfiguration;
 
 class TestCase extends SupportTestCase
 {
     use LoadTestConfiguration;
-
-    /**
-     * @param \Illuminate\Foundation\Application $app
-     *
-     * @return array
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            ServiceProvider::class,
-        ];
-    }
 }
