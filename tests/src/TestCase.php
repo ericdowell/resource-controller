f8d44041bf80b3169379912f1b78214745a2ec0f
Eric Dowell <eric@ericdowell.com>
2018-04-19 22:26:32 -0500

    Space.

--- tests/src/TestCase.php
+++ tests/src/TestCase.php
@@ -3,7 +3,6 @@
 namespace EricDowell\ResourceController\Tests;
 
 use Orchestra\Testbench\TestCase as SupportTestCase;
-
 use EricDowell\ResourceController\Tests\Traits\LoadTestConfiguration;
 
 class TestCase extends SupportTestCase
