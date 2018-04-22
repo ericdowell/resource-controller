b0208f933a2a360eaa5a12d4bc91363b1fbd6b2e
Eric Dowell <eric@ericdowell.com>
2018-04-21 19:36:02 -0500

    Helps if we query the database.

--- tests/src/Feature/UserTest.php
+++ tests/src/Feature/UserTest.php
@@ -54,7 +54,7 @@ class UserTest extends TestCase
 
         $response->assertRedirect(url(route('user.index')));
 
-        $this->assertNotInstanceOf(TestUser::class, TestUser::wherePassword($password));
+        $this->assertNotInstanceOf(TestUser::class, TestUser::wherePassword($password)->first());
 
         return TestUser::whereEmail($email)->first();
     }
