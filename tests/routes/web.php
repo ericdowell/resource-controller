cef6decd4948113c3db638ea3a555f8f20afbba7
Eric Dowell <eric@ericdowell.com>
2018-04-25 21:42:01 -0500

    Setting up to make more tests.

--- tests/routes/web.php
+++ tests/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-/*
- * Models
- */
 Route::resource('post', 'TestPostController');
+Route::group(['prefix' => 'user/password', 'group' => 'user.'], function () {
+    Route::get('{user}/edit', ['as' => 'password-edit', 'uses' => 'UserController@passwordEdit']);
+    Route::put('{user}', ['as' => 'password-update', 'uses' => 'UserController@passwordUpdate']);
+});
 Route::resource('user', 'TestUserController');
--- tests/src/Http/Controllers/TestUserController.php
+++ tests/src/Http/Controllers/TestUserController.php
@@ -9,6 +9,14 @@ use EricDowell\ResourceController\Http\Controllers\ResourceModelController;
 class TestUserController extends ResourceModelController
 {
     use WithoutModelRequest;
+    /**
+     * Given a route action (key) set the form action (value).
+     *
+     * @var array
+     */
+    protected $actionMap = [
+        'password-edit' => 'password-update',
+    ];
 
     /**
      * @var bool
@@ -21,4 +29,50 @@ class TestUserController extends ResourceModelController
      * @var string
      */
     protected $modelClass = TestUser::class;
+
+    /**
+     * @param int $id
+     *
+     * @return Response
+     * @throws Throwable
+     */
+    public function passwordEdit($id): Response
+    {
+        return $this->edit($id);
+    }
+
+    /**
+     * @param UserRequest $request
+     * @param int $id
+     *
+     * @return RedirectResponse
+     */
+    public function passwordUpdate(UserRequest $request, $id): RedirectResponse
+    {
+        $user = $this->findModel($id);
+        $currentPassword = $request->input('current_password');
+
+        if (! Hash::check($currentPassword, $user->password)) {
+            return redirect()->back()->withErrors(['current_password' => 'Current password provided is incorrect.']);
+        }
+        $attributes = $request->all();
+        $attributes['password'] = Hash::make($attributes['password']);
+
+        $user->update($this->getModelAttributes($user, $attributes, true));
+
+        return $this->finishAction('update');
+    }
+
+    /**
+     * Updates attributes based on request for Eloquent Model.
+     *
+     * @param Request $request
+     * @param Model $instance
+     *
+     * @return bool
+     */
+    protected function updateAction(Request $request, Model $instance): bool
+    {
+        return $instance->update($this->getModelAttributes($instance, $request->all(), true)) ?? false;
+    }
 }
