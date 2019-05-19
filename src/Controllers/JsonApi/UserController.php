46ba7d701ded160d027a13145e07fa55aebfa659
Eric Dowell <github@ericdowell.com>
2019-05-19 12:01:45 -0500

    Removing some duplicate code and removing unneeded controllers.

--- src/Controllers/JsonApi/UserController.php
+++ src/Controllers/JsonApi/UserController.php
@@ -4,28 +4,11 @@ declare(strict_types=1);
 
 namespace ResourceController\Controllers\JsonApi;
 
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Foundation\Http\FormRequest;
 use ResourceController\Traits\Controllers\PaginateIndex;
+use ResourceController\Traits\Controllers\Response\WithJson;
+use ResourceController\Controllers\UserController as Controller;
 
-class UserController extends AbstractModelController
+class UserController extends Controller
 {
-    use PaginateIndex;
-
-    /**
-     * Get attributes from validated request.
-     *
-     * @param  \Illuminate\Foundation\Http\FormRequest  $request
-     * @return array
-     */
-    protected function getRequestAttributes(FormRequest $request): array
-    {
-        $attributes = parent::getRequestAttributes($request);
-
-        if (isset($attributes['password'])) {
-            $attributes['password'] = Hash::make($attributes['password']);
-        }
-
-        return $attributes;
-    }
+    use PaginateIndex, WithJson;
 }
