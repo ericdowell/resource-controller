724f905280e5d0f3a154a010a618a316fc12d1d8
Eric Dowell <github@ericdowell.com>
2019-05-19 11:44:32 -0500

    Removing options from WithJson method signatures.

--- src/Traits/Controllers/Response/WithJson.php
+++ src/Traits/Controllers/Response/WithJson.php
@@ -10,12 +10,11 @@ trait WithJson
      * @param  string|array|object  $data
      * @param  int  $status
      * @param  array  $headers
-     * @param  int  $options
      * @return \Symfony\Component\HttpFoundation\Response|\Illuminate\Http\JsonResponse
      */
-    protected function response($data = [], $status = 200, array $headers = [], $options = 0)
+    protected function response($data = [], $status = 200, array $headers = [])
     {
-        return response()->json($data, $status, $headers, $options);
+        return response()->json($data, $status, $headers);
     }
 
     /**
@@ -24,12 +23,11 @@ trait WithJson
      * @param  array  $data
      * @param  int  $status
      * @param  array  $headers
-     * @param  int  $options
      * @return \Symfony\Component\HttpFoundation\Response|\Illuminate\Http\JsonResponse
      */
-    protected function responseModifySuccess($data = [], $status = 200, array $headers = [], $options = 0)
+    protected function responseModifySuccess($data = [], $status = 200, array $headers = [])
     {
-        return $this->response($data, $status, $headers, $options);
+        return $this->response($data, $status, $headers);
     }
 
     /**
@@ -38,12 +36,11 @@ trait WithJson
      * @param  array  $data
      * @param  int  $status
      * @param  array  $headers
-     * @param  int  $options
      * @return \Symfony\Component\HttpFoundation\Response|\Illuminate\Http\JsonResponse
      */
-    protected function responseDestroySuccess($data = [], $status = 200, array $headers = [], $options = 0)
+    protected function responseDestroySuccess($data = [], $status = 200, array $headers = [])
     {
-        return $this->response($data, $status, $headers, $options);
+        return $this->response($data, $status, $headers);
     }
 
     /**
@@ -52,11 +49,10 @@ trait WithJson
      * @param  array  $data
      * @param  int  $status
      * @param  array  $headers
-     * @param  int  $options
      * @return \Symfony\Component\HttpFoundation\Response|\Illuminate\Http\JsonResponse
      */
-    protected function responseError($data = [], $status = 200, array $headers = [], $options = 0)
+    protected function responseError($data = [], $status = 200, array $headers = [])
     {
-        return $this->response($data, $status, $headers, $options);
+        return $this->response($data, $status, $headers);
     }
 }
