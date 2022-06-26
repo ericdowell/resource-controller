816a2577ce4affe6454027f94b7229a3bbfd5a9f
Eric Dowell <github@ericdowell.com>
2022-06-26 00:47:24 -0500

    Update CI Image (#37)
    * Updating ci image to cimg/php:8.1.
    
    * Updating styleci.
    
    * Apply fixes from StyleCI
    
    * Updating ci to version 2.1.
    
    * Adding pecl install xdebug to ci.
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- tests/src/Models/TestPost.php
+++ tests/src/Models/TestPost.php
@@ -11,12 +11,14 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property-read \ResourceController\Tests\Models\TestUser $user
  * @mixin \Illuminate\Database\Eloquent\Builder
+ *
  * @property int $id
  * @property string $title
  * @property string $body
  * @property int $test_user_id
  * @property \Carbon\Carbon|null $created_at
  * @property \Carbon\Carbon|null $updated_at
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|\ResourceController\Tests\Models\TestPost whereBody($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\ResourceController\Tests\Models\TestPost whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\ResourceController\Tests\Models\TestPost whereId($value)
